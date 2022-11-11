5c7538005993b99a7b0acd895940922d41cd5c68
Daniel <69319302+DanielHenderson-17@users.noreply.github.com>
2022-11-10 23:10:35 -0600

    eco new

---
+++ assets/js/servers.js
@@ -0,0 +1,39 @@
+FETCHDATATIMEOUT = 3 * 60 * 1000; // 3 minutes
+
+function fetchData(serverId) {
+    fetch(`https://ecoservers.io/api/?object=servers&element=detail&key=${serverId}`)
+        .then(r => r.json())
+        .then(data => printData(serverId, data))
+    setTimeout(() => fetchData(serverId), FETCHDATATIMEOUT);
+}
+
+function printData(serverId, data) {
+    const serverElement = document.querySelector(`#server-${serverId}`);
+    serverElement.querySelector('.serverName').innerText = data.name;
+    serverElement.querySelector('.version').innerText = data.version;
+    serverElement.querySelector('.playerCount').innerText = data.players;
+    serverElement.querySelector('.voteLink').href = `${data.url}/vote`;
+    serverElement.querySelector('.connect').href = `steam://connect/${data.address}:${data.port}`;
+    const onlineDot = serverElement.querySelector('.onlineDot');
+    if(data.is_online == "1") {
+        onlineDot.classList.add("green");
+    } else {
+        onlineDot.classList.remove("green");
+    }
+}
+
+function addServer(serverId) {
+    const template = document.querySelector('#ServerTemplate');
+    const serverListDiv = document.querySelector('#serverList');
+    const clone = template.content.cloneNode(true);
+    const serverDiv = clone.querySelector('.serverContainer');
+    serverDiv.id = `server-${serverId}`;
+    serverListDiv.appendChild(serverDiv);
+    fetchData(serverId);
+}
+
+const servers = [
+    "XRFRT7ybbqb4TbYwrVA47zldAVUnfpoVHSz", // Eco 
+];
+servers.forEach(server => addServer(server));
+
